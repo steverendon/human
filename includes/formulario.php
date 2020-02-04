96ada7024391de754a3f54a28d2543687c7736eb
steven rendon <steven.rendon@hotmail.com>
2020-02-03 20:02:47 -0500

    se modifica letras del formulario

--- includes/formulario.php
+++ includes/formulario.php
@@ -1,8 +1,8 @@
 <div class="start-form position-fixed text-center content-form">
   <div class="bg-white rounded">
     <img src="img/image43.png" alt="logo de la empresa" width="210">
-    <p class="h5 text-uppercase font-weight-bold"><b><span>certificacion</span><br><span> en coaching profesional</span></b></p>
-    <p class="m-2" style="font-size: 0.8em;">Conoce más sobre nuestro programa de formación, déjanos tus datos.</p>
+    <center><p class="h5 text-uppercase font-weight-bold w-75"><b><span style="font-size:1.1em; color: #8EB7C1">certificacion</span><br><span style="font-size:0.8em"> en coaching profesional</span></b></p><center>
+    <p class="m-2 w-75 m-auto" style="font-size: 0.8em;">Conoce más sobre nuestro programa de formación, déjanos tus datos.</p>
     <form class="row my-form mx-2">
         <div class="col-6">
             <div class="form-group">
